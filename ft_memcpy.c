821b5ce42530123515c6ec489ff9ad9dad6d5b50
Simão Bonvalot <73948790+izzypt@users.noreply.github.com>
2022-12-14 21:35:46 +0000

    Add files via upload

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: smagalha <smagalha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 13:23:44 by smagalha          #+#    #+#             */
-/*   Updated: 2022/12/09 20:01:38 by smagalha         ###   ########.fr       */
+/*   Updated: 2022/12/14 20:39:18 by smagalha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char	*p;
 	unsigned char	*d;
 
+	if (!dest && !src)
+		return (NULL);
 	p = (unsigned char *) src;
 	d = (unsigned char *) dest;
 	while (n > 0)
